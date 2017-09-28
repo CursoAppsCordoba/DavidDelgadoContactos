0e05813db3edebbfc1725208a7fcbda1f93ae72f
seatclk <seatclk@users.noreply.github.com>
2017-09-28 09:04:56 +0200

    actualizado

---
+++ app/src/main/java/com/example/david/contactos/EditarActivity.java
@@ -0,0 +1,15 @@
+package com.example.david.contactos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar);
+
+
+    }
+}
--- app/src/main/java/com/example/david/contactos/ListviewActivity.java
+++ app/src/main/java/com/example/david/contactos/ListviewActivity.java
@@ -1,6 +1,9 @@
 package com.example.david.contactos;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListviewActivity extends AppCompatActivity {
+    private static AlertDialog ventana;
     private ListView listView;
     private List<Contacto> listac = new ArrayList<>();
     private List<String> lista = new ArrayList<>();
@@ -28,18 +32,41 @@ public class ListviewActivity extends AppCompatActivity {
         Intent intent = getIntent();
         listac = (ArrayList) intent.getParcelableArrayListExtra("listaContactos");
         for (Contacto c : listac) {
-            lista.add(c.getNombre()+" "+c.getEmail()+" "+c.getEdad());
+            lista.add(c.getNombre() + " " + c.getEmail() + " " + c.getEdad());
         }
 
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,lista);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, lista);
         listView.setAdapter(adapter);
-       listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(ListviewActivity.this, lista.get(i), Toast.LENGTH_LONG).show();
+              if(ventana == null) {
+                  ventana = CreateDialog();
+
+              }
+              ventana.show();
             }
         });
+    }
 
+
+    public AlertDialog CreateDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("No hago el editar por que no se ");
+        builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int id) {
+                Intent intent1 = new Intent(ListviewActivity.this,EditarActivity.class);
+                startActivityForResult (intent1 ,Activity.RESULT_CANCELED );
+
+            }
+        });
+        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+        return builder.create();
     }
 
 
